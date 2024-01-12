ed3c0b00800d3001974b563c8be095797c29fb54
sunithadhannani <142322613+sunithadhannani@users.noreply.github.com>
2024-01-12 10:33:23 +0530

    Update sunitha.py

--- sunitha.py
+++ sunitha.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.header('puppy')
 st.write('puppy')
-st.image('https://www.bing.com/images/search?view=detailV2&ccid=M6acAhue&id=D49B437388184C246105BFA953FA1EAA6AE7DCE2&thid=OIP.M6acAhueNKPF0Ou5vKYfpwAAAA&mediaurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.33a69c021b9e34a3c5d0ebb9bca61fa7%3frik%3d4tznaqoe%252blOpvw%26riu%3dhttp%253a%252f%252ffarm5.staticflickr.com%252f4112%252f5170590074_066e255655_o.jpg%26ehk%3dO0UGl8xtb23IwppKhPr3hxxjacGWKBlO2iAPY1fwbY4%253d%26risl%3d%26pid%3dImgRaw%26r%3d0&exph=376&expw=474&q=puppy&simid=608027194917864009&FORM=IRPRST&ck=3A58E26A9F0E3F1CED454423CD942FE7&selectedIndex=7&itb=0&ajaxhist=0&ajaxserp=0',caption='puppy')
+st.image('https://th.bing.com/th/id/R.33a69c021b9e34a3c5d0ebb9bca61fa7?rik=4tznaqoe%2blOpvw&riu=http%3a%2f%2ffarm5.staticflickr.com%2f4112%2f5170590074_066e255655_o.jpg&ehk=O0UGl8xtb23IwppKhPr3hxxjacGWKBlO2iAPY1fwbY4%3d&risl=&pid=ImgRaw&r=0',caption='puppy')
