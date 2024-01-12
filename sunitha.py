4f6043bb3b78db86d83365474048d10d619cc2ef
sunithadhannani <142322613+sunithadhannani@users.noreply.github.com>
2024-01-12 11:05:53 +0530

    Update sunitha.py

--- sunitha.py
+++ sunitha.py
@@ -2,4 +2,3 @@ import streamlit as st
 st.header('puppy')
 st.write('puppy')
 st.image('https://th.bing.com/th/id/R.33a69c021b9e34a3c5d0ebb9bca61fa7?rik=4tznaqoe%2blOpvw&riu=http%3a%2f%2ffarm5.staticflickr.com%2f4112%2f5170590074_066e255655_o.jpg&ehk=O0UGl8xtb23IwppKhPr3hxxjacGWKBlO2iAPY1fwbY4%3d&risl=&pid=ImgRaw&r=0',caption='puppy')
-st.video('https://youtu.be/t0Q2otsqC4I?si=0cAYSeukUkNXKb6S',caption='Tom&Jerry')
